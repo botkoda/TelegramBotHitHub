9b04c6ea2c3c4eeb0eb47cbe87bec4d5d49f7372
botkoda <botkoda@gmail.com>
2021-12-09 19:32:40 +0400

    telegramBot added

--- src/main/java/GitHubJob.java
+++ src/main/java/GitHubJob.java
@@ -10,75 +10,79 @@ import java.util.Map;
 public class GitHubJob {
     GitHub github;
     GHRepository repo;
-    Map<Integer, String> issueMap = new HashMap<>();
+
 
     {
         try {
-            github = new GitHubBuilder().withOAuthToken("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").build();
+            github = new GitHubBuilder().withOAuthToken("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").build();
             repo = github.getRepository("noviygorod1k/task-tracking");
-            List<GHIssue> issuesList = getOpenIssues(repo);
-            //все открытые таски
-            issuesList.stream().forEach(x -> {
-                issueMap.put(x.getNumber(), x.getTitle());
-            });
-            //коменты конкретного таска;
-            List<GHIssueComment> issuesCommentList= getCommentsIssues(findIssue(88,issuesList)); 
-            //вывод в консоль    
-            issueMap.forEach((k,v)-> System.out.println(k+" "+v));
-            issuesCommentList.stream().forEach(x-> System.out.println(issuesCommentList.indexOf(x)+"-->"+x.getBody()));
+
+
+//            getOpenIssues().forEach((k, v) -> System.out.println(k + " " + v));
+//            getCommentsIssues(88).forEach(x -> System.out.println(x));
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public List<GHIssue> getOpenIssues(GHRepository ghRepository) throws IOException {
-//          List<GHIssue> issues = ghRepository.listIssues(GHIssueState.ALL)
-//       .withPageSize(1000)
-//       .asList();
-        List<GHIssue> issues = ghRepository.getIssues(GHIssueState.OPEN);
-        List<GHIssue> answer = new ArrayList<>();
-        for (GHIssue issue : issues) {
-            answer.add(issue);
-        }
-        return answer;
-    }
-    
-    public GHIssue findIssue(int number, List<GHIssue> issues) {
-        //GHIssue issue=issues.Stream().map(issue->issue.getNumber()).filter(x->x==number).findAny().get()
-        
-    if (issues != null) {
-        for (GHIssue issue : issues) {
-            if (issue.getNumber() == number) {
-                return issue;
+    public Map<Integer, String> getOpenIssues() {
+        Map<Integer, String> issueMap = new HashMap<>();
+        //  List<GHIssue> answer = new ArrayList<>();
+
+        //  List<GHIssue> issues = null;
+        if (repo != null) {
+            try {
+                List<GHIssue> issues = repo.getIssues(GHIssueState.OPEN);
+                issues.stream().forEach(x -> issueMap.put(x.getNumber(), x.getTitle()));
+                return issueMap;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
         }
-    }
-    return null;
-}
- 
+        return issueMap;
 
-    public List<GHIssueComment> getCommentsIssues(GHIssue issue) {
-//         GHIssue issue = new GHIssue();
-//         List<GHIssueComment> comments = null;
-//         for (GHIssue i : issuesList) {
-//             if (i.getNumber() == number) {
-//                 issue = i;
-//             }
-//         }
-        if (issue != null) {
+    }
+
+    private GHIssue findIssue(int number) {
         try {
-           List<GHIssueComment> comments = issue.getComments();
-           return comments;
+            List<GHIssue> issues = repo.getIssues(GHIssueState.OPEN);
+            if (issues != null) {
+                for (GHIssue issue : issues) {
+                    if (issue.getNumber() == number) {
+                        return issue;
+                    }
+                }
+            }
         } catch (IOException e) {
             e.printStackTrace();
-        }   
         }
+
         return null;
     }
 
-    public void createIssue(GHRepository ghRepository,String lable){
-        ghRepository.createIssue(lable);
 
+    public List<String> getCommentsIssues(int number) {
+        GHIssue issue = findIssue(number);
+        List<String> listIssueWithComment = new ArrayList<>();
+        if (issue != null) {
+            try {
+                listIssueWithComment.add("Тема: " +issue.getNumber()+"."+issue.getTitle());
+                listIssueWithComment.add("Проблема: " + issue.getBody());
+                for (var l : issue.getComments()) {
+                    listIssueWithComment.add("Коммент: " + l.getBody());
+                }//.stream().map(x->listIssueWithComment.add("Комментарии:" +x.getBody() ));
+                return listIssueWithComment;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return listIssueWithComment;
+    }
+
+    public GHIssueBuilder createIssue(GHRepository ghRepository, String lable) {
+        GHIssueBuilder ghIssueBuilder = ghRepository.createIssue(lable);
+        return ghIssueBuilder;
     }
 }
